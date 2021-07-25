8a84b8428067b49a143f46ebba699b34fb9d204a
AMakidon <richalex@mail.ru>
2021-07-25 15:47:05 +0300

    Initial commit

---
+++ app/src/main/java/second/homeworkfirst/MainActivitySecond.java
@@ -0,0 +1,13 @@
+package second.homeworkfirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivitySecond extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_second);
+    }
+}
